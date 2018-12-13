2ec06b70d53900a1c81a7d604cbb44c16e924d3b
Mustafa Türk <mustafa.turk@student.ehb.be>
2018-12-14 00:53:55 +0100

    Test?

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,29 +1,64 @@
 var data;
+var steps;
 var totalSteps = 0;
 
-var fontSize = 30;
+var canvasWidth = 800;
+var canvasHeight = 800;
+var fontSize = 26;
 
 function preload() {
     data = loadJSON('data.json')
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(canvasWidth, canvasHeight);
+
     textSize(fontSize);
-    var steps = data.steps;
-    for (var i = 0; i < steps.length; i++) {
-        var date = new Date();
-        totalSteps += Math.floor(steps[i].steps);
-    }
-    console.log(totalSteps)
+
+    steps = data.steps;
+    
+    //Functions
+    calculateSteps();
+    drawRects();
 }
 
 function draw() {
     textAlign(CENTER);
-    drawWords();
+    drawWords(width * .5);
+    fill(0)
 }
 
 function drawWords(x) {
     fill(0);
-    text("I walked " + totalSteps + " steps this year.", windowWidth / 2, 80);
+    text("I walked " + totalSteps + " steps this year.", canvasWidth / 2, 80);
+}
+
+function calculateSteps() {
+    for (var i = 0; i < steps.length; i++) {
+        var date = new Date();
+        totalSteps += Math.floor(steps[i].steps);
+    }
+}
+
+function drawRects() {
+    var newX = 100;
+    var newY = 200;
+    var margin = 20;
+    var index = 0;
+
+    for(var i = 1; i < steps.length; i++) {
+        if(Math.floor(steps[i].steps) > 1000) {
+            fill(0)
+        } else {
+            fill(255)
+        }
+
+        rect(newX, newY, 10, 10);
+
+        if (i % 30 == 0) {
+            newX = 80;
+            newY += 50;
+        }
+        newX += margin;
+    }
 }
---
+++ Assignment 5/README.md
@@ -0,0 +1,11 @@
+# DATAVIZ
+
+## Assignment 1
+
+## Assignment 2
+
+## Assignment 3
+
+## Assignment 4
+
+## Assignment 5
