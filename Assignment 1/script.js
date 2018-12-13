9ec0b5c094d4057781e5c17ae18f6a223c688fe2
Mustafa Türk <mustafa.turk@student.ehb.be>
2018-12-13 23:27:58 +0100

    Display the total steps on the canvas

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,10 +1,29 @@
 var data;
+var totalSteps = 0;
+
+var fontSize = 30;
 
 function preload() {
     data = loadJSON('data.json')
 }
 
-function setup () {
-    var steps_data = data.steps;
-    console.log(steps_data.length);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    textSize(fontSize);
+    var steps = data.steps;
+    for (var i = 0; i < steps.length; i++) {
+        var date = new Date();
+        totalSteps += Math.floor(steps[i].steps);
+    }
+    console.log(totalSteps)
+}
+
+function draw() {
+    textAlign(CENTER);
+    drawWords();
+}
+
+function drawWords(x) {
+    fill(0);
+    text("I walked " + totalSteps + " steps this year.", windowWidth / 2, 80);
 }
