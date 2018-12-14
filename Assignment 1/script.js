1a2348089bccd507baee233a0446270d161991f8
Mustafa Türk <mustafa.turk@student.ehb.be>
2018-12-14 01:22:53 +0100

    Added colors to the rects, according to the stepcount

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -12,9 +12,9 @@ function preload() {
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
-
     textSize(fontSize);
 
+    //Assign the steps array from the JSON to steps var
     steps = data.steps;
     
     //Functions
@@ -24,6 +24,8 @@ function setup() {
 
 function draw() {
     textAlign(CENTER);
+
+    //Draw the text in center of canvas
     drawWords(width * .5);
     fill(0)
 }
@@ -35,7 +37,7 @@ function drawWords(x) {
 
 function calculateSteps() {
     for (var i = 0; i < steps.length; i++) {
-        var date = new Date();
+        //Round the step values and add them to the totalSteps
         totalSteps += Math.floor(steps[i].steps);
     }
 }
@@ -44,21 +46,31 @@ function drawRects() {
     var newX = 100;
     var newY = 200;
     var margin = 20;
-    var index = 0;
 
     for(var i = 1; i < steps.length; i++) {
-        if(Math.floor(steps[i].steps) > 1000) {
-            fill(0)
-        } else {
-            fill(255)
+        noStroke();
+        //Change the colors according to the stepcount, higher = darker
+        if (Math.floor(steps[i].steps) <= 500) {
+            fill(174, 213, 231)
         }
-
+        
+        if (Math.floor(steps[i].steps) >= 500) {
+            fill(95, 113, 237)
+        }
+        
+        if (Math.floor(steps[i].steps) >= 5000) {
+            fill(44, 39, 244)
+        }
+        
         rect(newX, newY, 10, 10);
 
+        //If the rect is the 30th in the row, set the y lower and begin from the left side again
         if (i % 30 == 0) {
             newX = 80;
             newY += 50;
         }
+
+        //Add margin to the rect
         newX += margin;
     }
 }
